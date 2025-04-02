0b3542f4e887893c8f96ceccc440d6459856eca1
Anton Jackson-Smith <anton@dyingstar.net>
2025-04-02 13:06:13 -0700

    oops

--- bin/setup-stub.sh
+++ bin/setup-stub.sh
@@ -4,5 +4,5 @@ LOG_FILE=/home/jovyan/work/.log/`date -Iseconds`-setup.log
 
 echo "In setup-stub: logging to $LOG_FILE"
 
-mkdir -p `basename $LOG_FILE`
+mkdir -p `dirname $LOG_FILE`
 /opt/repo/bin/setup.sh | tee -a ${LOG_FILE}
