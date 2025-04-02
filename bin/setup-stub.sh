5014fbd0b673e8f42349a83964f6f6992d0c8a6f
Anton Jackson-Smith <anton@dyingstar.net>
2025-04-02 13:02:01 -0700

    Alter setup.sh loading to write a permanent log to the user's volume

---
+++ bin/setup-stub.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+LOG_FILE=/home/jovyan/work/.log/`date -Iseconds`-setup.log
+
+echo "In setup-stub: logging to $LOG_FILE"
+
+mkdir -p `basename $LOG_FILE`
+/opt/repo/bin/setup.sh | tee -a ${LOG_FILE}
