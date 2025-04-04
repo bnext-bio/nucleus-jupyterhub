263fbe55aba716bf1a3ad41a3a70dbf8cf6f6564
Anton Jackson-Smith <anton@dyingstar.net>
2025-04-04 16:41:32 -0700

    don't print commands

--- bin/curvenote-submit.sh
+++ bin/curvenote-submit.sh
@@ -2,7 +2,7 @@
 
 # Provide a devnote selector because Jupyter App Launcher doesn't have an easy way to operated
 # in the CWD selected in the file browser.
-set -exo pipefail
+set -eo pipefail
 
 cd ~/work/devnotes
 cd `ls -d ./*/ | ~/.fzf/bin/fzf`
