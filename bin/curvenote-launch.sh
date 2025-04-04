173ac2e74f1dac3eb26ab26e7267eeb0b0c00923
Anton Jackson-Smith <anton@dyingstar.net>
2025-04-04 16:48:03 -0700

    pre-warm curvenote preview

--- bin/curvenote-launch.sh
+++ bin/curvenote-launch.sh
@@ -2,13 +2,18 @@
 
 CURVENOTE_BUILD=/opt/repo/bin/curvenote-build.sh
 HTML_DIR=~/work/devnotes/.html
+LOG_FILE=~/work/.log/curvenote.log
 
-echo "============" | tee -a ~/work/.log/curvenote.log
-echo `date -Iseconds` | tee -a ~/work/.log/curvenote.log
-echo "Launching curvenote preview proxy" | tee -a ~/work/.log/curvenote.log
-echo "BASE_URL_PREFIX: ${BASE_URL_PREFIX}" | tee -a ~/work/.log/curvenote.log
+echo "============" | tee -a ${LOG_FILE}
+echo `date -Iseconds` | tee -a ${LOG_FILE}
+echo "Launching curvenote preview proxy" | tee -a ${LOG_FILE}
+echo "BASE_URL_PREFIX: ${BASE_URL_PREFIX}" | tee -a ${LOG_FILE}
 
 cd ~/work/devnotes/
 mkdir -p $HTML_DIR
-caddy file-server --listen :$1 --browse --root $HTML_DIR/ | tee -a ~/work/.log/curvenote.log &
-nodemon -w ~/work/devnotes/ -i _build/ -i .html/ --ext md,yml,json,ipynb,html -x $CURVENOTE_BUILD | tee -a ~/work/.log/curvenote.log &
+
+# Prelaunch curvenote build so stuff is ready when we get redirected to the website
+/bin/bash -c "${CURVENOTE_BUILD}" | tee -a ${LOG_FILE}
+
+caddy file-server --listen :$1 --browse --root $HTML_DIR/ | tee -a ${LOG_FILE} &
+nodemon -w ~/work/devnotes/ -i _build/ -i .html/ --ext md,yml,json,ipynb,html -x $CURVENOTE_BUILD | tee -a ${LOG_FILE} &
