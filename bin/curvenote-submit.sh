cfcd5f55f715f354ba585a32a7fce844e39c6f81
Anton Jackson-Smith <anton@dyingstar.net>
2025-04-04 16:38:09 -0700

    add fzf path

--- bin/curvenote-submit.sh
+++ bin/curvenote-submit.sh
@@ -2,7 +2,8 @@
 
 # Provide a devnote selector because Jupyter App Launcher doesn't have an easy way to operated
 # in the CWD selected in the file browser.
+set -exo pipefail
 
 cd ~/work/devnotes
-cd `ls -d ./*/ | fzf`
+cd `ls -d ./*/ | ~/.fzf/bin/fzf`
 curvenote submit bnext-devnotes --collection nucleus-contrib $1
