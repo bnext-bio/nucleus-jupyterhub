0fe71439f7001bf4d8dc00b1e75ed2ccc1adaea4
Anton Jackson-Smith <anton@dyingstar.net>
2025-04-09 12:26:37 -0700

    Curvenote submit token warning

--- bin/curvenote-submit.sh
+++ bin/curvenote-submit.sh
@@ -4,6 +4,11 @@
 # in the CWD selected in the file browser.
 set -eo pipefail
 
+if [ ! -f ~/.curvenote/settings.json ]; then
+    echo "Please set up your curvenote token."
+    echo "Go to https://curvenote.com/"
+fi
+
 cd ~/work/devnotes
 cd `ls -d ./*/ | ~/.fzf/bin/fzf`
 curvenote submit bnext-devnotes --yes --collection nucleus-contrib $1
