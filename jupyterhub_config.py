6735109be29f43689eaa44cc7df612565580b105
Anton Jackson-Smith <anton@dyingstar.net>
2025-04-22 13:40:53 -0700

    Test if appending template paths is a bad idea

--- jupyterhub_config.py
+++ jupyterhub_config.py
@@ -77,7 +77,7 @@ c.Authenticator.allow_all = True
 
 # Authenticate users with Native Authenticator
 c.JupyterHub.authenticator_class = "nativeauthenticator.NativeAuthenticator"
-c.JupyterHub.template_paths.append(f"{os.path.dirname(nativeauthenticator.__file__)}/templates/")
+c.JupyterHub.template_paths = [f"{os.path.dirname(nativeauthenticator.__file__)}/templates/"]
 
 # Allow anyone to sign-up without approval
 c.NativeAuthenticator.open_signup = False
