a8457b9b409de9154d4f586eae1b0a2a726dd865
Anton Jackson-Smith <anton@dyingstar.net>
2025-04-23 14:37:35 -0700

    export relevant env vars

--- bin/setup-stub.sh
+++ bin/setup-stub.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-REPO=/opt/repo
-LOG_FILE=/home/jovyan/work/.log/`date -Iseconds`-setup.log
+export REPO=/opt/repo
+export LOG_FILE=/home/jovyan/work/.log/`date -Iseconds`-setup.log
 
 echo "In setup-stub: logging to $LOG_FILE"
 mkdir -p `dirname $LOG_FILE`
@@ -10,6 +10,7 @@ echo "Updating git repository: ${REPO}" | tee ${LOG_FILE}
 if [ ! -d ${REPO} ]; then
     git clone --depth=1 ${GIT_REMOTE} ${REPO} |& tee ${LOG_FILE}
 fi
+
 cd ${REPO}
 git remote set-url origin ${GIT_REMOTE} |& tee ${LOG_FILE} # Fix up remote if image was built from a repo with an SSH origin.
 git pull |& tee ${LOG_FILE}
