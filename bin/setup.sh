e2113ad2ad798eaee771a1f1668232a62e7b6d7a
Anton Jackson-Smith <anton@dyingstar.net>
2025-04-23 15:17:22 -0700

    quote collab title

--- bin/setup.sh
+++ bin/setup.sh
@@ -30,7 +30,7 @@ echo "Creating collaboration launchers for user groups"
 for group in `curl -H "Authorization: token $JUPYTERHUB_API_TOKEN" $JUPYTERHUB_API_URL/user | jq -r '.groups | join("\n")'`; do
     echo "Creating launcher for group: ${group}"
     echo """
-- title: Collab: ${group}
+- title: "Collab: ${group}"
   description: Open the real-time collaboration server for ${group}
   source: /hub/spawn/${group}-collab
   type: url
