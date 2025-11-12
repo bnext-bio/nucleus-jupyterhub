145097c52453a74c76aa55cd80850879d527fa8d
Anton Jackson-Smith <anton@dyingstar.net>
2025-11-11 21:20:53 -0800

    Set jupyter server root dir to user home

--- config/jupyter_server_config_additional.py
+++ config/jupyter_server_config_additional.py
@@ -1,6 +1,8 @@
-
 # Additional configuration for the nucleus jupyter environment
 # c.Application.log_level = "DEBUG"
+
+# Launch in home directory
+c.ServerApp.root_dir = "/home/jovyan"
 
 # Extra node roots so we can install the language server
 c.LanguageServerManager.extra_node_roots = ["/opt/noderoots"]
@@ -8,7 +10,10 @@ c.LanguageServerManager.extra_node_roots = ["/opt/noderoots"]
 c.LabApp.custom_css = True
 
 c.JupyterLabTemplates.allowed_extensions = ["*.ipynb"]
-c.JupyterLabTemplates.template_dirs = ["/opt/repo/templates", "/home/jovyan/shared/templates"]
+c.JupyterLabTemplates.template_dirs = [
+    "/opt/repo/templates",
+    "/home/jovyan/shared/templates",
+]
 c.JupyterLabTemplates.include_default = False
 c.JupyterLabTemplates.include_core_paths = True
 c.JupyterLabTemplates.template_label = "Template"
@@ -21,19 +26,17 @@ c.ServerProxy.servers = {
     "curvenote-start": {
         "command": ["/bin/bash", "/opt/repo/bin/curvenote-launch.sh", "{port}"],
         "absolute_url": False,
-        "environment": {
-            "BASE_URL_PREFIX": "{base_url}curvenote-start"
-        },
+        "environment": {"BASE_URL_PREFIX": "{base_url}curvenote-start"},
         "timeout": 120,
         "launcher_entry": {
             "enabled": True,
             "title": "Curvenote Preview",
             # "category": "Nucleus",
-            "icon_path": "/opt/repo/share/curvenote-logo.svg"
+            "icon_path": "/opt/repo/share/curvenote-logo.svg",
         },
-        "new_browser_tab": False
+        "new_browser_tab": False,
     }
 }
 
 # Set default terminal shell
-c.NotebookApp.terminado_settings = { "shell_command": ["/bin/zsh"] }
+c.NotebookApp.terminado_settings = {"shell_command": ["/bin/zsh"]}
