c0d42ff685cb1dd63d0a7ae4642ea7dc4da8b150
Anton Jackson-Smith <anton@dyingstar.net>
2025-12-02 12:44:42 -0800

    Remove path selector

--- bin/curvenote-submit.sh
+++ bin/curvenote-submit.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
-# Provide a devnote selector because Jupyter App Launcher doesn't have an easy way to operated
-# in the CWD selected in the file browser.
 set -eo pipefail
 
 if [ ! -f ~/.curvenote/settings.json ]; then
     echo "Please set up your curvenote token."
-    echo "Go to https://curvenote.com/"
+    echo "Go to https://editor.curvenote.com/profile"
 fi
 
-cd ~/work/devnotes
-cd `ls -d ./*/ | fzf`
 curvenote submit bnext-devnotes --yes --collection nucleus-contrib $1
