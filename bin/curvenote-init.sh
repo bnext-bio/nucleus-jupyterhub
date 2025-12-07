f602ce2f047d778a55ee9d1671baabe2a9aacc31
Anton Jackson-Smith <anton@dyingstar.net>
2025-12-06 20:22:00 -0800

    Workaround bug with `curvenote init --output`

--- bin/curvenote-init.sh
+++ bin/curvenote-init.sh
@@ -8,4 +8,7 @@ read "dirname?Enter a directory name for the DevNote: "
 dirname="$(echo $dirname | tr '[:upper:]' '[:lower:]' | sed -r "s/[^[:alnum:]]/-/g" | sed -r "s/-+/-/g" | sed -r "s/-+$//g")"
 echo "Creating DevNote in ${dirname}"
 
-curvenote init --github https://github.com/antonrmolina/devnote-template --output ${dirname}
+mkdir -p "${dirname}"
+cd "${dirname}"
+
+curvenote init --github https://github.com/antonrmolina/devnote-template
