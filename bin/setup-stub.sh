855424c9b9e8ae20a9f7386caff5637742b17fea
Anton Jackson-Smith <anton@dyingstar.net>
2026-04-21 15:03:42 +0100

    Provide git status in log before attempting update

--- bin/setup-stub.sh
+++ bin/setup-stub.sh
@@ -15,6 +15,7 @@ if [ ! -d ${REPO} ]; then
 fi
 
 cd ${REPO}
+git status |& tee -a ${LOG_FILE}
 git remote set-url origin ${GIT_REMOTE} |& tee -a ${LOG_FILE} # Fix up remote if image was built from a repo with an SSH origin.
 git checkout ${BRANCH}
 git pull |& tee -a ${LOG_FILE}
