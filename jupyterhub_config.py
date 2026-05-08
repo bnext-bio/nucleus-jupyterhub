1f6cada7f3e7e676bde6fee711b46f7062545af1
Anton Jackson-Smith <anton@dyingstar.net>
2026-05-08 00:07:56 -0700

    Set run_as_root to allow change user

--- jupyterhub_config.py
+++ jupyterhub_config.py
@@ -49,6 +49,7 @@ c.GenericOAuthenticator.admin_groups = {"hub-admins"}
 
 c.JupyterHub.spawner_class = "dockerspawner.SystemUserSpawner"
 c.SystemUserSpawner.host_homedir_format_string = '/mnt/ssd/users/{username}'
+c.SystemUserSpawner.run_as_root = True
 
 c.DockerSpawner.image = os.environ["HUB_NOTEBOOK_IMAGE"]
 c.DockerSpawner.start_timeout = 300
