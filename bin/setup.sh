049200087fca61640ee3221b4fd2cac8d130ce25
Anton Jackson-Smith <anton@dyingstar.net>
2026-05-08 01:24:10 -0700

    copy overlay with normal user perms

--- bin/setup.sh
+++ bin/setup.sh
@@ -16,7 +16,7 @@ cd ${REPO}
 
 # Bring down and update our baseline home directory
 echo "Updating home directory overlay."
-rsync -a ${REPO}/home-overlay/ ${HOME}
+cp -R ${REPO}/home-overlay/ ${HOME}
 
 # Install shell basics if necessary
 zsh -ci "source /opt/antidote/antidote.zsh && antidote load"
