09611d3d127487199b930b16cb7a94413f66e161
Anton Jackson-Smith <anton@dyingstar.net>
2026-05-09 14:23:14 -0700

    still chown the repo that's easier

--- jupyterhub_config.py
+++ jupyterhub_config.py
@@ -75,7 +75,8 @@ c.SystemUserSpawner.environment = {
     'NB_GID':          '{groupid}',    # ditto if your version exposes it; otherwise hardcode or compute
     'CHOWN_HOME':      'yes',
     'CHOWN_HOME_OPTS': '',
-    'CHOWN_EXTRA': '/opt/conda,/opt/repo,/opt/noderoots',
+    'CHOWN_EXTRA': '/opt/repo,/opt/noderoots',
+    'CHOWN_EXTRA_OPTS': '-R',
     "JUPYTER_RUNTIME_DIR": "/tmp/jupyter-runtime" # Move runtime outside of mounted volume, so restarts clean up runtime info
     # Critically: do NOT set CHOWN_EXTRA to include the bind-mounted host path
 }
