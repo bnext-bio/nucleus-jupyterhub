fd989f85e6b816e80a7f5adbd637f9b163544bfa
Anton Jackson-Smith <anton@dyingstar.net>
2026-05-09 14:35:57 -0700

    recursive chown absolutely wrecks docker launch

--- jupyterhub_config.py
+++ jupyterhub_config.py
@@ -76,7 +76,6 @@ c.SystemUserSpawner.environment = {
     'CHOWN_HOME':      'yes',
     'CHOWN_HOME_OPTS': '',
     'CHOWN_EXTRA': '/opt/repo,/opt/noderoots',
-    'CHOWN_EXTRA_OPTS': '-R',
     "JUPYTER_RUNTIME_DIR": "/tmp/jupyter-runtime" # Move runtime outside of mounted volume, so restarts clean up runtime info
     # Critically: do NOT set CHOWN_EXTRA to include the bind-mounted host path
 }
