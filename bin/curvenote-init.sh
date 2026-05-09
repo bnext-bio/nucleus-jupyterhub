cebf644067c216e997a21473c58337d0e7ed3ee8
Anton Jackson-Smith <anton@dyingstar.net>
2026-05-08 18:30:51 -0700

    Create devnote in whatever dir we're already in

--- bin/curvenote-init.sh
+++ bin/curvenote-init.sh
@@ -2,8 +2,6 @@
 
 set -euo pipefail
 
-cd /home/jovyan/devnotes
-
 read "dirname?Enter a directory name for the DevNote: "
 dirname="$(echo $dirname | tr '[:upper:]' '[:lower:]' | sed -r "s/[^[:alnum:]]/-/g" | sed -r "s/-+/-/g" | sed -r "s/-+$//g")"
 echo "Creating DevNote in ${dirname}"
