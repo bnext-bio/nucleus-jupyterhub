be31d543439fb6c470b4800e528110bbf71b42b2
Anton Jackson-Smith <anton@dyingstar.net>
2026-05-09 20:06:52 -0700

    more fix

--- bin/copyparty.sh
+++ bin/copyparty.sh
@@ -3,4 +3,4 @@
 set -uo pipefail
 
 cd $HOME
-uvx --with pillow copyparty -c ~/.config/copyparty.conf --rp-loc ${JUPYTERHUB_SERVICE_PREFIX}proxy/absolute/3923
+uvx --with pillow copyparty -c ~/.config/copyparty.conf --rp-loc ${JUPYTERHUB_SERVICE_PREFIX}proxy/absolute/3923 |& tee /tmp/copyparty-`date -Iseconds`.log
