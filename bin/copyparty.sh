477360a92799b1f309a4230e14c5ac7e20665c36
Anton Jackson-Smith <anton@dyingstar.net>
2026-05-09 20:42:54 -0700

    add more deps

--- bin/copyparty.sh
+++ bin/copyparty.sh
@@ -3,4 +3,12 @@
 set -uo pipefail
 
 cd $HOME
-uvx --with pillow copyparty -c ~/.config/copyparty.conf -p $1 --name "${USER} hub" --rp-loc ${JUPYTERHUB_SERVICE_PREFIX}copyparty/ |& tee /tmp/copyparty-`date -Iseconds`.log
+uvx \
+  --with pillow \
+  --with pyvips \
+  --with mutagen \
+  copyparty
+  -c ~/.config/copyparty.conf \
+  -p $1 --name "${USER} hub" \
+  --rp-loc ${JUPYTERHUB_SERVICE_PREFIX}copyparty/ \
+  |& tee /tmp/copyparty-`date -Iseconds`.log
