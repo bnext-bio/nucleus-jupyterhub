e17d8f3825d0905ec0d506eac605d148f2eb5b75
Anton Jackson-Smith <anton@dyingstar.net>
2026-05-09 20:01:13 -0700

    pillow dep for copyparty

--- bin/copyparty.sh
+++ bin/copyparty.sh
@@ -3,4 +3,4 @@
 set -uo pipefail
 
 cd $HOME
-uvx --with ffmpeg copyparty -c ~/.config/copyparty.conf --rp-loc ${JUPYTERHUB_SERVICE_PREFIX}proxy/absolute/3923
+uvx --with pillow copyparty -c ~/.config/copyparty.conf --rp-loc ${JUPYTERHUB_SERVICE_PREFIX}proxy/absolute/3923
