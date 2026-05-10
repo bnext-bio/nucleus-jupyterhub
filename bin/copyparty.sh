e69c31ef770042047f2f48ff3abbab96f96b84cc
Anton Jackson-Smith <anton@dyingstar.net>
2026-05-09 20:24:08 -0700

    custom copyparty hostname

--- bin/copyparty.sh
+++ bin/copyparty.sh
@@ -3,4 +3,4 @@
 set -uo pipefail
 
 cd $HOME
-uvx --with pillow copyparty -c ~/.config/copyparty.conf -p $1 --rp-loc ${JUPYTERHUB_SERVICE_PREFIX}copyparty/ |& tee /tmp/copyparty-`date -Iseconds`.log
+uvx --with pillow copyparty -c ~/.config/copyparty.conf -p $1 --name "${USER} hub" --rp-loc ${JUPYTERHUB_SERVICE_PREFIX}copyparty/ |& tee /tmp/copyparty-`date -Iseconds`.log
