1b95cd5e0d056d52e9792be6fcdd7363b0134276
Anton Jackson-Smith <anton@dyingstar.net>
2026-05-11 00:31:50 -0700

    tempt fate by making containers ephermeral

--- jupyterhub_config.py
+++ jupyterhub_config.py
@@ -118,7 +118,7 @@ c.DockerSpawner.notebook_dir = "~/"
 #     "nucleushub-user-{username}": notebook_dir
 # }
 
-c.DockerSpawner.remove = False
+c.DockerSpawner.remove = True
 c.DockerSpawner.debug = True
 
 # Permissions for sharing / RTC
